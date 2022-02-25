0e080d3b81cd4a4f6e6fd79b69c46ad8967c636a
m0hit2736 <90374382+m0hit2736@users.noreply.github.com>
2022-02-25 16:31:39 +0530

    Add files via upload

---
+++ CaseStudy4/Book.java
@@ -0,0 +1,35 @@
+package com.ac.bean;
+
+public class Book {
+	//variables for book
+	  private int bookId;
+	  private String booktitle;
+	  private float bookprice;
+	  private String bookgrade;
+	  
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getBooktitle() {
+		return booktitle;
+	}
+	public void setBooktitle(String booktitle) {
+		this.booktitle = booktitle;
+	}
+	public float getBookprice() {
+		return bookprice;
+	}
+	public void setBookprice(float bookprice) {
+		this.bookprice = bookprice;
+	}
+	public String getBookgrade() {
+		return bookgrade;
+	}
+	public void setBookgrade(String bookgrade) {
+		this.bookgrade = bookgrade;
+	}
+	  
+}
